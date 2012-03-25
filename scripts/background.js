b842136a23403c3b1512e30134ed50a495294a42
Alistair Ballantine <aballant01@gmail.com>
2012-03-25 17:46:13 -0400

    Adding duration calculation when a tab is closed and adding event handling when a tab is updated

--- scripts/background.js
+++ scripts/background.js
@@ -1,24 +1,28 @@
 
 var app = (function(){
-    if(localStorage['web_informatics_data']){
-        data = JSON.parse(localStorage['web_informatics_data']);
-    }else{
-        data = {};
-        data['history'] = [];
-        data['bookmarks'] = [];
-        data['indices'] = {};
-    }
 
-    base_urls = [
-        "facebook.com",
-        "www.google.com/search",
-        "mail.google.com",
-        "reddit.com",
-        "plus.google.com"
-    ];
+    var init = function(){
+        if(localStorage['web_informatics_data']){
+            data = JSON.parse(localStorage['web_informatics_data']);
+        }else{
+            data = {};
+            data['history'] = [];
+            data['bookmarks'] = [];
+            data['indices'] = {};
+            data['start_time'] = Date.now();
+        }
+    }   
 
-    var re = /^http[s]?:\/\/([a-zA-Z\.-]+)/;
+    var re = /^http[s]?:\/\/([0-9a-zA-Z\.-]+)/;
 
+    /**
+    * Takes a complete url path and returns the core domain
+    * path, AKA the base URL. For example, the URL
+    * http://www.foo.com/bar?test=true would return www.foo.com
+    *
+    * @param {url} The complete url string
+    * @returns The base URL component of the full URL
+    */
     var getBaseURL = function( url ){
         var path = url.split("/");
         if(path.length > 1){
@@ -30,10 +34,20 @@ var app = (function(){
         };   
     };
 
+    /**
+    * Stores the current iteration of the app data into HTML5 
+    * local storage as a JSON string
+    */
     var storeData = function(){
         localStorage['web_informatics_data'] = JSON.stringify(data);
     };
 
+    /**
+    * Adds an array index corresponding to a History item to 
+    * the corresponding base URL index map, app.data.indices
+    * @param {base_url} The base url of the site being visited
+    * @param {index} The history index of the added item
+    */
     var addIndexCount = function( base_url, index ) {
         if(data.indices[base_url]){
             data.indices[base_url].push(index);
@@ -49,17 +63,20 @@ var app = (function(){
     */
     var storeInterval = function(){
         window.setTimeout(function(){
-            console.log("Interval");
             storeData();
             storeInterval();
         }, 60000);
     };
 
+    /**
+    * Clears the data stored in LocalStorage
+    */
     var clearData = function(){
         data = {};
         storeData();
     };
 
+    init();
     storeInterval();
 
     return {
@@ -95,18 +112,53 @@ var tab = (function(){
     });
 
     chrome.tabs.onUpdated.addListener(function( tabId, changeInfo, tab ){
+        console.log("Updated")
         console.log(tab);
-        console.log(changeInfo);
-        console.log(tab);
+
+        if(tab.status === "complete"){
+
+            var lastTab = findLastTab(data.history, tabId);
+            if (lastTab.url === tab.url){
+                return;
+            }else{
+
+                lastTab.setDuration(Date.now());
+
+                var histitem = new history.HistItem( tab );
+                var index = app.data.history.push(histitem);
+                app.addIndexCount(histitem.baseURL, index);
+            }
+        }
+
     });
 
     chrome.tabs.onRemoved.addListener(function(tab, removeInfo){
-        // For this, we will need to retrieve the tab data from the id
-        // passed in the "tab" parameter
-        console.log(tab);
-        console.log(removeInfo);
+        // Find the most recent occurence of the Tab ID
+        var historyItem = findLastTab( data.history, tab);
+
+        historyItem.setDuration(Date.now());
+        // just doing it this way for now, will try for better way 
     });
 
+    /**
+    * 
+    */
+    var findLastTab = function( arr, tabId, url ){
+        for(var i = arr.length; i > 0; i--){
+
+            var tab = arr[i-1];
+            
+            if(tab.ID === tabId){
+                if(url){
+                    if(app.getBaseURL(tab.url) !== app.getBaseURL(url)){
+                        continue;
+                    }
+                }
+                return tab;
+            }
+        }
+        return 0;
+    }
 })();
 
 /**
@@ -120,16 +172,19 @@ var tab = (function(){
 */
 var history = (function(){
     var HistItem = function( obj ) {
-        this.index = app.data.history.length + 1;
+        this.index = app.data.history.length + 1 || obj.id;
         this.ID = obj.id;
         this.url = obj.url;
         this.baseURL = app.getBaseURL(obj.url);
-        this.startTime = (new Date()).getTime();
+        this.startTime = Date.now();
         // Initially set to 0 - when closed or updated add to the duration
         this.duration = 0;
-        this.chromeObj = obj;
+        //this.chromeObj = obj;
     };
 
+    /*
+    * Append the duration of time spent on this 
+    */
     HistItem.prototype.setDuration = function( endTime ){
         this.duration = endTime - this.startTime; 
     };
@@ -139,7 +194,7 @@ var history = (function(){
     */
     HistItem.prototype.getDuration = function(){
         var dur = this.duration;
-        return (dur !== 0) ? dur : (new Date()).getTime() - this.startTime;
+        return (dur !== 0) ? dur : Date.now() - this.startTime;
     };
 
     /**
